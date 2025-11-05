17c32209916fc7fb08d6fb8c448ee2df2d989f76
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-11-05 16:31:29 -0300

    actualizar campo de fecha_creacion

--- todoList/models.py
+++ todoList/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Tarea(models.Model):
     titulo = models.CharField(max_length=50)
-    fecha_creacion = models.DateField()
+    fecha_creacion = models.DateField(auto_now_add=True) #Fecha del sistema al momento de crear el objeto
     descripcion = models.TextField()
     estado = models.CharField(max_length=30)
