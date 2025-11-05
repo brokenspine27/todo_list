550b3f40fb656745c92c220ad194341968183935
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-11-05 16:27:52 -0300

    creacion del modelo tarea

--- todoList/models.py
+++ todoList/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=50)
+    fecha_creacion = models.DateField()
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=30)
